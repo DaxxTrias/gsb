94ef828b4c2a88eb7892a2c8c56a0ccad40cd090
Sean <sean.daxxtrias@gmail.com>
2024-06-06 04:09:40 -0500

    code gardening

--- physUtils.cpp
+++ physUtils.cpp
@@ -8,47 +8,51 @@ inline bool cmpf(float A, float B, float epsilon = 0.005f) {
 }
 
 std::vector<bodyData> generateBodyData() {
-	std::vector<bodyData> bodys;
+    std::vector<bodyData> bodys;
 
-	if (physList != nullptr) {
-		for (int i = 0; i < 0xFFFFFF; i++) {
-			if (physList[i].entry != nullptr
-				&& (physList[i].id & 0xFFFFFF) == i
-				&& ((physList[i].entry->id & 0xFFFFFF) == (physList[i].id & 0xFFFFFF))) {
-				physx::PxActor* actor = physList[i].entry->actor;
-				if (actor == nullptr) {
-					continue;
-				}
-				physx::PxRigidActor* rigid = actor->is<physx::PxRigidActor>();
-				if (rigid == nullptr || (uint64_t)rigid > 0xFFFF'FFFF'FFFF'0000) {
-					continue;
-				}
-				physx::PxVec3 pos;
-				if (rigid->getGlobalPose().isValid()) {
-					pos = rigid->getGlobalPose().p;
-				}
-				else {
-					continue;
-				}
-				bool isBody = actor->is<physx::PxRigidBody>() != nullptr;
-				bool isStatic = actor->is<physx::PxRigidBody>() != nullptr;
-				float mass = -1;
-				if (isBody) {
-					physx::PxRigidBody* body = actor->is<physx::PxRigidBody>();
-					mass = body->getMass();
-					bodys.push_back({ pos, mass });
-				}
-			}
-			else {
-				if ((physList[i].id & 0xFFFFFF) != (i & 0xFFFFFF)) {
-					break;
-				}
-			}
-		}
-	}
+    if (physList != nullptr) {
+        for (int i = 0; i < 0xFFFFFF; i++) {
+            if (physList[i].entry != nullptr
+                && (physList[i].id & 0xFFFFFF) == i
+                && ((physList[i].entry->id & 0xFFFFFF) == (physList[i].id & 0xFFFFFF))) {
 
-	return bodys;
+                physx::PxActor* actor = physList[i].entry->actor;
+                if (actor == nullptr) {
+                    continue;
+                }
+
+                physx::PxRigidActor* rigid = actor->is<physx::PxRigidActor>();
+                if (rigid == nullptr || (uint64_t)rigid > 0xFFFF'FFFF'FFFF'0000) {
+                    continue;
+                }
+
+                physx::PxVec3 pos;
+                if (rigid->getGlobalPose().isValid()) {
+                    pos = rigid->getGlobalPose().p;
+                }
+                else {
+                    continue;
+                }
+
+                bool isStatic = actor->is<physx::PxRigidBody>() != nullptr;
+                bool isBody = actor->is<physx::PxRigidBody>() != nullptr;
+                if (isBody) {
+                    physx::PxRigidBody* body = actor->is<physx::PxRigidBody>();
+                    float mass = body->getMass();
+                    bodys.push_back({ pos, mass });
+                }
+            }
+            else {
+                if ((physList[i].id & 0xFFFFFF) != (i & 0xFFFFFF)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return bodys;
 }
+
 
 bodyData getPlyByMass(std::vector<bodyData>& bodys) {
     for (bodyData body : bodys) {
@@ -56,6 +60,5 @@ bodyData getPlyByMass(std::vector<bodyData>& bodys) {
             return body;
         }
     }
-    // Add a default return statement in case no matching body is found
-    return {};
+    return {}; // Return a default-constructed bodyData if no match is found
 }
