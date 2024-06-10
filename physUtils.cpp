d9b846d03e676c68f8b51a07e12f4e9521ff88c9
Sean <sean.daxxtrias@gmail.com>
2024-06-10 01:45:06 -0500

    update todo notes about get player func (should really rename from 'ply')

--- physUtils.cpp
+++ physUtils.cpp
@@ -77,6 +77,8 @@ std::vector<bodyData> generateBodyData() {
 //todo: we should investigate if this player location shuffles around
 // im assuming it does, but if is more static maybe we can cache the data to not double loop the entity list
 // everytime we look for an entity and the player, every frame
+//followup: first position in ent table looks like player and doesnt appear to move.
+// 2nd entrat looks a lot like personal ship (pawnVehicle?)
 bodyData getPlyByMass(std::vector<bodyData>& bodys) {
     for (bodyData body : bodys) {
         if (cmpf(body.mass, 2.287f, 0.01f)) {
