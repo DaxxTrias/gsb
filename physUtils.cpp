7a990c833fd5f54bc8405b094b6ece9bce3c625c
Sean <sean.daxxtrias@gmail.com>
2024-06-10 20:25:28 -0500

    mitigating a crash that slipped through from refactor

--- physUtils.cpp
+++ physUtils.cpp
@@ -24,11 +24,12 @@ std::vector<bodyData> generateBodyData() {
         for (uint32_t i = 0; i < maxObjects; ++i) {
             auto& entry = physList[i];
 
-            if (entry.entry == nullptr ||
-                (entry.id & 0xFFFFFF) != i ||
-                ((entry.entry->id & 0xFFFFFF) != (entry.id & 0xFFFFFF))) {
+            if (entry.entry == nullptr)
                 continue;
-            }
+
+            if ((entry.id & 0xFFFFFF) != i ||
+                ((entry.entry->id & 0xFFFFFF) != (entry.id & 0xFFFFFF)))
+                continue;
 
             physx::PxActor* actor = entry.entry->actor;
             if (actor == nullptr) {
