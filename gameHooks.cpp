13349ef53870f36829d4fce24a526933096d134f
Sean <sean.daxxtrias@gmail.com>
2024-07-23 00:42:42 -0500

    v1000052

--- gameHooks.cpp
+++ gameHooks.cpp
@@ -267,7 +267,9 @@ uintptr_t camObjectOffset = 0xAEB46A8; // v922: F2 0F 11 05 ? ? ? ? 41 8B 41
 //uintptr_t camObjectOffsetSTU = 0xAE2C438; // v1000046
 //uintptr_t camObjectOffsetSTU = 0xAE2C3F0; // v1000047
 //uintptr_t camObjectOffsetSTU = 0xAE2D408; // v1000048
-uintptr_t camObjectOffsetSTU = 0xAE2F5A0; // v1000050
+//uintptr_t camObjectOffsetSTU = 0xAE2F5A0; // v1000050
+uintptr_t camObjectOffsetSTU = 0xAE2F3D8; // v1000052
+
 uintptr_t camObject;
 
 void initGameHooks() {
