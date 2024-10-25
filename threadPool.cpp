b5a881a9c92a84d383b07653ed6daf3ad473798a
Sean <DaxxTrias@users.noreply.github.com>
2024-10-25 02:45:06 -0500

    wow it actually compiles now?

--- threadPool.cpp
+++ threadPool.cpp
@@ -35,24 +35,4 @@ ThreadPool::~ThreadPool() {
     }
 }
 
-template<class F>
-auto ThreadPool::enqueue(F&& f) -> std::future<typename std::invoke_result<F>::type> {
-    using returnType = typename std::invoke_result<F>::type;
-
-    auto task = std::make_shared<std::packaged_task<returnType()>>(std::forward<F>(f));
-
-    std::future<returnType> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(queueMutex);
-
-        if (stop) {
-            throw std::runtime_error("enqueue on stopped ThreadPool");
-        }
-
-        tasks.emplace_back([task]() { (*task)(); });
-    }
-    condition.notify_one();
-    return res;
-}
-
 ThreadPool threadPool(std::thread::hardware_concurrency());
--- threadPool.h
+++ threadPool.h
@@ -15,7 +15,24 @@ public:
     ~ThreadPool();
 
     template<class F>
-    auto enqueue(F&& f) -> std::future<typename std::invoke_result<F>::type>;
+    auto enqueue(F&& f) -> std::future<typename std::invoke_result<F>::type> {
+        using returnType = typename std::invoke_result<F>::type;
+
+        auto task = std::make_shared<std::packaged_task<returnType()>>(std::forward<F>(f));
+
+        std::future<returnType> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+
+            if (stop) {
+                throw std::runtime_error("enqueue on stopped ThreadPool");
+            }
+
+            tasks.emplace_back([task]() { (*task)(); });
+        }
+        condition.notify_one();
+        return res;
+    }
 
 private:
     std::vector<std::thread> workers;
@@ -27,3 +44,7 @@ private:
 };
 
 extern ThreadPool threadPool;
+
+// Explicit instantiation declarations
+template auto ThreadPool::enqueue<std::function<void()>>(std::function<void()>&&) -> std::future<void>;
+template auto ThreadPool::enqueue<std::function<int()>>(std::function<int()>&&) -> std::future<int>;
